8665acf9e68c41664190d6a7bc6260e7662179ec
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-08 16:07:39 -0400

    Internal clean_session function
    To be used in tests.

--- R/session.r
+++ R/session.r
@@ -10,3 +10,10 @@
   unlockBinding("_session", asNamespace("pkgconfig"))
   invisible()
 }
+
+clean_session <- function(pkg) {
+  session <- get("_session")
+  if (exists(pkg, where = session)) {
+    rm(list = pkg, envir = session)
+  }
+}
